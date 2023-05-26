f3e556eeb7fa0ba81ce31e0e814eb9c23ade8bd7
iceman2146 <iceman2146@mail.ru>
2023-05-27 01:01:24 +0300

    ничо не робит

--- src/ip_filter.cpp
+++ src/ip_filter.cpp
@@ -1,61 +1,45 @@
 #include <cassert>
 #include <cstdlib>
 #include <iostream>
-#include <string>
 #include <vector>
+#include <algorithm>
+#include "IP_lib.h"
 
-
-
-auto split(const std::string &str, char d)
-{
-    std::vector<std::string> r;
-
-    std::string::size_type start = 0;
-    std::string::size_type stop = str.find_first_of(d);
-    while(stop != std::string::npos)
-    {
-        r.push_back(str.substr(start, stop - start));
-
-        start = stop + 1;
-        stop = str.find_first_of(d, start);
-    }
-
-    r.push_back(str.substr(start));
-
-    return r;
-}
+template<typename T, typename ... Args>
+auto ip_address(T number, Args ... args){
+    ip result = ip_address(args...);
+    result.push_back(number);
+    std::reverse(result.begin(), result.end());
+    return result;
+};
 
 int main()
 {
-    std::cout<<"IP_filter"<<std::endl;
-        std::vector<std::vector<std::string> > ip_pool;
-        std::cout<<ip_pool.size();
+    try
+    {
+        std::vector<ip> ip_pool;
 
-        for(std::string line; std::getline(std::cin, line);)
-        {
-            
-            std::vector<std::string> v = split(line, '\t');
-            std::cout<<line;
-            ip_pool.push_back(split(v.at(0), '.'));
+        for(std::string line; std::getline(std::cin, line);){
+            auto v = split(line, '\t');
+            ip_pool.push_back(to_int(split(v.at(0), '.')));
         }
+
+        std::sort(ip_pool.begin(), ip_pool.end(), [](auto a, auto b) { return (a > b); });
+
+        std::cout << ip_pool;
+
+        ip_filter(ip_pool, ip_address(1));
+       
+        ip_filter(ip_pool, ip_address(46, 70));
+
+        ip_filter_any(ip_pool, 46);
+
         
-
-        for(auto ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
-        {
-            
-            for(auto ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part)
-            {
-                std::cout << __PRETTY_FUNCTION__ << std::endl;
-                if (ip_part != ip->cbegin())
-                {
-                    std::cout << __PRETTY_FUNCTION__ << std::endl;
-                    std::cout << ".";
-
-                }
-                std::cout << *ip_part;
-            }
-            std::cout << std::endl;
-        }
+    }
+    catch(const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
 
     return 0;
 }
